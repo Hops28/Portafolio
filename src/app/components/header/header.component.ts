51c13f313a2aebaee8efc70afeeb5081dafbd138
Hops <jasson.jjmc@gmail.com>
2023-02-01 22:42:32 -0600

    Creado el fondo de inicio

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,83 @@
 import { Component, OnInit } from '@angular/core';
+import {  HostListener } from "@angular/core";
+
+
+declare var window: Window;
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
 
-  constructor() { }
+export class HeaderComponent implements OnInit
+{
+  // arreglo donde se guardará en valores numéricos para el retraso de la animación de las esferas
+  retraso: number[] = [];
+
+  datos = {
+    burbujas : 25,
+    ancho : 1300
+  }
+
+  // Consructor
+  constructor() {
+
+    // En el constructor solo asignamos el retraso de las burbujas que aparecen (valores entre 10 y 30)
+    this.edit_burbujas(this.datos.burbujas, 1)
+  }
 
   ngOnInit(): void {
   }
 
+  /*************** FUNCIONES PROPIAS ***********************/
+  @HostListener("window:resize", ["$event"])
+  onResize(event: Event) {
+    this.datos.ancho = window.innerWidth;
+    this.datos.burbujas = Math.floor(this.datos.ancho / 50);
+
+    this.edit_burbujas(this.datos.burbujas, 2);
+  }
+
+  // Función para modificar el arreglo de burbujas
+  edit_burbujas (burbujas : number, tipo : number){
+    // Si el tipo es 1, entonces se generan las burbujas
+    if (tipo == 1)
+    {
+      for(let i = 0; i < burbujas; i++)
+      {
+        this.retraso.push(Math.floor(Math.random() * 40) + 10);
+      }
+    }
+    else if (tipo == 2) // Si es de tipo 2 entonces es de modificación
+    {
+      if(burbujas > this.retraso.length) // Si lo calculado es mayor a las burbujas actuales entonces se agregan más
+      {
+        let burbujas_faltantes  = burbujas - this.retraso.length;
+
+        for(let i = 0; i < burbujas_faltantes; i++)
+        {
+          // Se agrega un valor aleatorio, ese valor significa la duración de la animación
+          this.retraso.push(Math.floor(Math.random() * 40) + 10);
+        }
+      }
+      else if (burbujas < this.retraso.length) // Y si es menor, entonces se eliminan algunas
+      {
+        let burbujas_eliminables  = this.retraso.length - burbujas;
+
+        for(let i = 0; i < burbujas_eliminables; i++)
+        {
+          // Se eliminan tantos valores como sean necesarios
+          this.retraso.pop();
+        }
+
+        console.log(this.retraso);
+      }
+    }
+  }
+
 }
+
+// window.addEventListener("resize", () => {
+//   var width = window.innerWidth;
+// });
